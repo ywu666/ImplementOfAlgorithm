263c25d4239a7ce5592b0d5feaa49adff3285a3d
ywu666 <ywu660@aucklanduni.ac.nz>
2019-12-19 12:37:50 +0800

    Add rotate algorithm without using other helper methods.

--- src/algorithem/Matrix.java
+++ src/algorithem/Matrix.java
@@ -47,6 +47,20 @@ public class Matrix {
 		}	
 	}
 
+	public static int[][] rotate(int[][] matrix) {
+		int n = matrix.length;
+		for (int i = 0; i < n / 2; i++) {
+			for (int j = 0; j < Math.ceil(((double) n) / 2.); j++) {
+				int temp = matrix[i][j];
+				matrix[i][j] = matrix[n-1-j][i];
+				matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+				matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+				matrix[j][n-1-i] = temp;
+			}
+		}
+		return matrix;
+	}
+
 	public static void rotateSquareImageCW(int[][] matrix) {
 		transportMatrix(matrix);
 		flipItVerticalAxis(matrix);		    
