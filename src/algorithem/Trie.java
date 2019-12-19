4d4d139d46123ef46e241f96a7f7c2ee8b8673d1
ywu666 <ywu660@aucklanduni.ac.nz>
2019-12-19 16:36:32 +0800

    Implement basic algorithms forTrie data structure.

--- src/algorithem/Trie.java
+++ src/algorithem/Trie.java
@@ -1,5 +1,14 @@
 package algorithem;
 
+import java.util.HashMap;
+
+/**
+ * A Trie or Prefix Tree an efficient data lookup structure - often used to store large
+ * collections of words or dictionaries. 
+ * With a Trie, search complexities can be reduced to O(k) where k is the key or word length.
+ * @author ywu
+ *
+ */
 class Trie {
 	private TrieNode root;
 
@@ -7,16 +16,57 @@ class Trie {
 	public Trie() {
 		this.root = new TrieNode();
 	}
-	
+
 	public void insertWord(String word) {
-		
+		if (word == null || word.isEmpty()) return ;
+		TrieNode curr = root;
+		HashMap<Character,TrieNode> children = new HashMap<>();
+
+		for (int i =0;i<word.length();i++) {
+			char ch = word.charAt(i);
+			if (children.containsKey(ch)) {
+				curr = children.get(ch);
+			} else {
+				TrieNode newNode= new TrieNode(ch);
+				children.put(curr.c, newNode);
+				curr = newNode;
+			}
+			children = curr.children;
+			if (i == word.length() - 1) curr.isLeaf = true;
+		}		
 	}
-	
+
 	public Boolean searchWord(String word) {
-		return null;
+		TrieNode curr = root;
+		HashMap<Character,TrieNode> children = root.children;
+		for (int i = 0;i<word.length();i++) {
+			char ch = word.charAt(i);
+			if (i == word.length()-1) curr.isLeaf = true;
+			if (children.containsKey(ch)) {
+				curr = children.get(ch);
+				children = curr.children;
+			}else {
+				return false;
+			}
+		}
+
+		return curr.isLeaf;
 	}
-	
+
 	public Boolean searchPrefix(String word) {
-		return null;
+		TrieNode curr = root;
+		HashMap<Character,TrieNode> childrens = root.children;
+
+		for (int i = 0; i < word.length(); i++) {
+			char ch = word.charAt(i);
+			if (i == word.length() - 1) curr.isLeaf = true;
+			if (childrens.containsKey(ch)) {
+				curr = childrens.get(ch);
+				childrens = curr.children;
+			} else {
+				return false;
+			}
+		}       
+		return true;  
 	}
 }
