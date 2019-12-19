ddecf4e644e149b92784d4d78d1c41daf46c41e2
ywu666 <ywu660@aucklanduni.ac.nz>
2019-12-19 16:39:41 +0800

    Add comment on TrieNode.java.

--- src/algorithem/TrieNode.java
+++ src/algorithem/TrieNode.java
@@ -2,6 +2,11 @@ package algorithem;
 
 import java.util.HashMap;
 
+/**
+ * TrieNode class that represents one node of a Trie, and a partially complete Trie class.
+ * @author ywu
+ *
+ */
 class TrieNode {
     Character c;
     Boolean isLeaf = false;
