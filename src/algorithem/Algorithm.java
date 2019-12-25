9759dd51c55b725f50f3ddf7e2e0105df5437173
ywu666 <ywu660@aucklanduni.ac.nz>
2019-12-25 11:50:38 +0800

    Add maxProfit algorithm for array.

--- src/algorithem/Algorithm.java
+++ src/algorithem/Algorithm.java
@@ -688,6 +688,16 @@ public class Algorithm {
 		}
 		return res;
 	}
+	
+	public static int maxProfit(int[] prices){
+	    int profit = 0;
+	    for(int i=1; i < prices.length; i++){
+	        if((prices[i] - prices[i-1]) > 0){
+	            profit+=(prices[i] - prices[i-1]);
+	        }
+	    }
+	    return profit;
+	}
 
 	/**
 	 * Given two Strings, a and b, write a method - editDistance that 
