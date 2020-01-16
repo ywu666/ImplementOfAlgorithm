61194883430c816cbcf7f8c10c95a7a2cf8c0acc
ywu666 <ywu660@aucklanduni.ac.nz>
2020-01-16 15:39:43 +0800

    Improve the elegence.

--- src/algorithem/StringAlgorithm.java
+++ src/algorithem/StringAlgorithm.java
@@ -12,7 +12,7 @@ public class StringAlgorithm {
 	public static String insertPairStar(String str) { //recursion
 		if(str == null || str.length() <= 1) return str;
 		return str.charAt(0) + 
-				(str.charAt(0) == str.charAt(1) ? "*" : "") +
+				(str.charAt(0) == str.charAt(1) ? "*":"") +
 				insertPairStar(str.substring(1));
 	}
 
@@ -34,7 +34,7 @@ public class StringAlgorithm {
 		if (str == null || str.isEmpty()) return str;
 		String inputStr = str;
 		String outputStr = "";
-		for (int i = inputStr.length() - 1; i >= 0;i--) {
+		for (int i = inputStr.length() - 1;i >= 0;i--) {
 			outputStr += inputStr.charAt(i);
 		}
 		return outputStr;
@@ -46,7 +46,7 @@ public class StringAlgorithm {
 			StringBuilder compress = new StringBuilder();
 			char lastChar = text.charAt(0);
 			int count = 1;
-			for (int i = 1; i < length; i++) {
+			for (int i = 1;i < length;i++) {
 				if (text.charAt(i) == lastChar) {		        	  
 					count++;
 				}else {
@@ -71,7 +71,7 @@ public class StringAlgorithm {
 		if (input1.length() != input2.length()) return false;
 
 		int[] buffer = new int[26]; 
-		for (int i = 0; i < input1.length(); i++) {
+		for (int i = 0;i < input1.length();i++) {
 			buffer[input1.charAt(i) - 'a']++;
 			buffer[input2.charAt(i) - 'a']--;
 		}
@@ -87,7 +87,7 @@ public class StringAlgorithm {
 	public static boolean isPalindrom(String str) {
 		if (str == null || str.equals("")) return true;
 
-		for (int i = 0; i < str.length()/2; i++) {
+		for (int i = 0; i < str.length() / 2; i++) {
 			if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
 				return false;
 			}
@@ -236,9 +236,9 @@ public class StringAlgorithm {
 
 		// Selectively mark all length 2 substrings as palindrome
 		// in a single pass.
-		for(int i = 0; i < len - 1; i++){
-			if(str.charAt(i) == str.charAt(i+1)){
-				memo[i][i+1] = true;
+		for(int i = 0;i < len - 1;i++){
+			if(str.charAt(i) == str.charAt(i + 1)){
+				memo[i][i + 1] = true;
 				maxSubstrLen = 2;
 				maxSubstrStartIndex = i;
 			}
@@ -248,10 +248,10 @@ public class StringAlgorithm {
 		// memo[l][] represents the starting
 		// character str.charAt(l) and memo[][l] represents the ending character
 		// in our check.
-		for(int l = 3; l <= len; l++){
+		for(int l = 3;l <= len;l++){
 			// Run the check until you reach the end of the String. i is the 
 			// start index.
-			for(int i = 0; i + l-1 < len; i++ ){
+			for(int i = 0;i + l - 1 < len;i++ ){
 				// j is the end index.
 				int j = i + l - 1;
 
@@ -259,7 +259,7 @@ public class StringAlgorithm {
 				// "abba" is a palindrome only if "bb" were a palindrome and "a" == "a"
 				// at the opposite ends of the String. Can be translated to the following
 				// condition using the memo pad:
-				if(memo[i+1][j-1] && str.charAt(i) == str.charAt(j)){
+				if(memo[i + 1][j - 1] && str.charAt(i) == str.charAt(j)){
 					memo[i][j] = true;
 					maxSubstrLen = l;
 					maxSubstrStartIndex = i;
@@ -279,7 +279,7 @@ public class StringAlgorithm {
 		}
 
 		char first = str.charAt(0);
-		permutations.add(first+""); //convert to string type
+		permutations.add(first + ""); //convert to string type
 		ArrayList<String> list = getCombPerms(str.substring(1));
 		for (String word: list) {
 			for (int i = 0;i <= word.length();i++) {
@@ -310,9 +310,9 @@ public class StringAlgorithm {
 		ArrayList<String> out = new ArrayList<>();
 		Deque<IpLevelNode> stack = new LinkedList<>();
 		//push 3 possibilities onto the stack
-		stack.addFirst(new IpLevelNode(0, input.substring(0,1),"",input.substring(1)));
-		stack.addFirst(new IpLevelNode(0, input.substring(0,2),"",input.substring(2)));
-		stack.addFirst(new IpLevelNode(0, input.substring(0,3),"",input.substring(3)));
+		stack.addFirst(new IpLevelNode(0,input.substring(0,1),"",input.substring(1)));
+		stack.addFirst(new IpLevelNode(0,input.substring(0,2),"",input.substring(2)));
+		stack.addFirst(new IpLevelNode(0,input.substring(0,3),"",input.substring(3)));
 		while(!stack.isEmpty()) {
 			IpLevelNode node = stack.removeFirst();
 			int currlevel = node.level;
@@ -329,7 +329,7 @@ public class StringAlgorithm {
 				String successor = remaining.substring(i);
 				if (IpToAppend.length() > 0) {
 					int num = Integer.parseInt(IpToAppend);
-					if (num <= 255) stack.addFirst(new IpLevelNode(currlevel + 1,IpToAppend ,predecessor,successor));
+					if (num <= 255) stack.addFirst(new IpLevelNode(currlevel + 1,IpToAppend,predecessor,successor));
 				}
 				i++;
 			}
@@ -339,12 +339,12 @@ public class StringAlgorithm {
 
 	public static int longestNRSubstringLen(String input) {
 		if (input == null || input.equals("")) return 0;
-		Hashtable<Character, Integer> charTable = new Hashtable<>();
+		Hashtable<Character,Integer> charTable = new Hashtable<>();
 		int i = 0, prev =0;
 
 		for (char ch:input.toCharArray()) {
 			if (!charTable.containsKey(ch)) {
-				charTable.put(ch, i++);
+				charTable.put(ch,i++);
 			}else {
 				prev = Math.max(prev,charTable.size());
 				//i = charTable.get(ch);
@@ -390,7 +390,7 @@ public class StringAlgorithm {
 			if (node.digitCount == len) {
 				out.add(node.word);
 			} else {
-				for (Character ch: mapping.get(digits.charAt(node.digitCount)).toCharArray()) {
+				for (Character ch:mapping.get(digits.charAt(node.digitCount)).toCharArray()) {
 					stack.addFirst(new PhoneNode(node.word + ch,node.digitCount + 1));
 				}
 			}
@@ -420,11 +420,11 @@ public class StringAlgorithm {
 			for (int j= 0;j <= lengthB;j++) {
 				char chb = b.charAt(j);
 				if (cha == chb) {
-					memo[i][j] = memo[i-1][j-1];
+					memo[i][j] = memo[i - 1][j - 1];
 				}else {
 					int replaceDist = memo[i][j];
-					int insertDist = memo[i][j-1] + 1;
-					int delateDist = memo[i-1][j] + 1;
+					int insertDist = memo[i][j - 1] + 1;
+					int delateDist = memo[i - 1][j] + 1;
 					int minDist = Math.min(replaceDist, Math.min(insertDist, delateDist));
 					memo[i][j] = minDist;
 				}
