430d2fe4198b72b8a0477cdc26b56ea8bf4fe20d
ywu666 <ywu660@aucklanduni.ac.nz>
2020-02-23 11:11:43 +0800

    Update valid ip expression.

--- src/algorithem/StringAlgorithm.java
+++ src/algorithem/StringAlgorithm.java
@@ -343,6 +343,25 @@ public class StringAlgorithm {
 		return out;
 	}
 
+	public String validIPAddress(String IP) { //use regular expression
+		if (IP == null) {
+			return "Neither";
+		}
+
+		String regex0 = "(\\d|[1-9]\\d|1\\d\\d|2[0-4]\\d|25[0-5])";
+		String regexIPv4 = regex0 + "(\\." + regex0 + "){3}";
+		String regex1 = "([\\da-fA-F]{1,4})";
+		String regexIPv6 = regex1 + "(:" + regex1 + "){7}";
+
+		String result = "Neither";
+		if (IP.matches(regexIPv4)) {
+			result = "IPv4";
+		} else if (IP.matches(regexIPv6)) {
+			result = "IPv6";
+		}
+		return result;
+	}
+	 
 	public static int longestNRSubstringLen(String input) {
 		if (input == null || input.equals("")) return 0;
 		HashMap<Character,Integer> charTable = new HashMap<>();
